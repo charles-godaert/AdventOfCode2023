bf4d59195bcbfb77c92b06141b58895deaa96f50
Charles Godaert <charles.godaert@etu.univ-amu.fr>
2023-12-05 19:53:48 +0100

    D4

---
+++ D3/Program.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace D3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputPath = "C:\\Users\\Charles\\Desktop\\Projects\\AdventOfCode2023\\D3\\input.txt";
+            // Match all numbers
+            string pattern = @"\d+";
+            // Detect all *
+            string patternForStar = @"\*";
+
+            List<string> lines = new List<string>();
+
+            int total = 0;
+            try
+            {
+                using (StreamReader sr = new StreamReader(inputPath))
+                {
+                    string line;
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        lines.Add(line);
+                    }
+                }
+
+                #region Part 2
+                foreach (string line in lines)
+                {
+                    List<int> adjacentNumbers = new List<int>();
+                    foreach (Match match in Regex.Matches(line, patternForStar))
+                    {
+                        adjacentNumbers.Clear();
+
+                        int starPosition = match.Index;
+
+                        // Check if numbers are adjacents to the star on the same line
+                        foreach (Match matchNumber in Regex.Matches(line, pattern))
+                        {
+                            int number = int.Parse(matchNumber.Value);
+                            int startNumberPositionInLine = matchNumber.Index;
+                            int endNumberPositionInLine = matchNumber.Index + matchNumber.Length - 1;
+
+                            if(startNumberPositionInLine == starPosition + 1 || endNumberPositionInLine == starPosition - 1)
+                            {
+                                adjacentNumbers.Add(number);
+                                continue;
+                            }
+                        }
+
+                        // Check if numbers are adjacents to the star on the previous line
+                        int previousLineNumber = lines.IndexOf(line) - 1;
+                        string previousLine = (previousLineNumber >= 0 ? lines[previousLineNumber] : String.Empty);
+                        if (previousLine != String.Empty)
+                        {
+                            // Get all numbers in the line
+                            foreach (Match matchNumber in Regex.Matches(previousLine, pattern))
+                            {
+                                int number = int.Parse(matchNumber.Value);
+                                int startNumberPositionInLine = matchNumber.Index;
+                                int endNumberPositionInLine = matchNumber.Index + matchNumber.Length - 1;
+
+                                if ((startNumberPositionInLine <= starPosition + 1  && startNumberPositionInLine >= starPosition - 1) || 
+                                    (endNumberPositionInLine   <= starPosition + 1  && endNumberPositionInLine   >= starPosition - 1))
+                                    
+                                {
+                                    adjacentNumbers.Add(number);
+                                    continue;
+                                }
+                            }
+                        }
+
+                        // Check if numbers are adjacents to the star on the next line
+                        int nextLineNumber = lines.IndexOf(line) + 1;
+                        string nextLine = (nextLineNumber < lines.Count ? lines[nextLineNumber] : String.Empty);
+                        if (nextLine != String.Empty)
+                        {
+                            // Get all numbers in the line
+                            foreach (Match matchNumber in Regex.Matches(nextLine, pattern))
+                            {
+                                int number = int.Parse(matchNumber.Value);
+                                int startNumberPositionInLine = matchNumber.Index;
+                                int endNumberPositionInLine = matchNumber.Index + matchNumber.Length - 1;
+
+                                if ((startNumberPositionInLine <= starPosition + 1 && startNumberPositionInLine >= starPosition - 1) ||
+                                    (endNumberPositionInLine <= starPosition + 1 && endNumberPositionInLine >= starPosition - 1))
+
+                                {
+                                    adjacentNumbers.Add(number);
+                                    continue;
+                                }
+
+                               
+                            }
+                        }
+
+                        if(adjacentNumbers.Count == 2)
+                        {
+                            total += adjacentNumbers[0] * adjacentNumbers[1];
+                        }
+                    }
+                }
+                #endregion Part 2
+
+                #region Part 1
+                /*
+                foreach(string line in lines)
+                {
+                    // Print number of lines
+                    //Console.WriteLine($"Line {lines.IndexOf(line)} : {line}");
+
+                    // Foreach number in the line
+                    foreach (Match match in Regex.Matches(line, pattern))
+                    {
+                        int number = int.Parse(match.Value);
+                        int startPositionInLine = match.Index;
+                        int endPositionInLine = match.Index + match.Length - 1;
+
+
+                        ////// Check if a special char (not a dot) is adjacent to the number
+                        
+                        // Current line
+                        if (startPositionInLine > 0 && line[startPositionInLine - 1] != '.' ||
+                            endPositionInLine < line.Length - 1 && line[endPositionInLine + 1] != '.')
+                        {
+                            total += number;
+                            //Console.WriteLine($"Number : {number} - Start position : {startPositionInLine} - End position : {endPositionInLine}");
+                            continue;
+                        }
+
+                        // Previous line
+                        int previousLineNumber = lines.IndexOf(line) - 1;
+                        string previousLine = (previousLineNumber >= 0 ? lines[previousLineNumber] : String.Empty);
+                        if (previousLine != String.Empty)
+                        {
+                            int previousLineSubstringStartIndex = startPositionInLine == 0 ? 0 : startPositionInLine - 1;
+                            int previousLineSubstringEndIndex = endPositionInLine == previousLine.Length - 1 ? endPositionInLine : endPositionInLine + 1;
+                            string previousLineSubstring = previousLine.Substring(previousLineSubstringStartIndex, previousLineSubstringEndIndex - previousLineSubstringStartIndex + 1);
+
+                            // Check if contains a special char (all char except dot or number)
+                            if (Regex.IsMatch(previousLineSubstring, @"[^\.0-9]"))
+                            {
+                                total += number;
+                                //Console.WriteLine($"Number : {number} - Start position : {startPositionInLine} - End position : {endPositionInLine}");
+                                continue;
+                            }
+                        }
+
+                        // Next line
+                        int nextLineNumber = lines.IndexOf(line) + 1;
+                        string nextLine = (nextLineNumber < lines.Count ? lines[nextLineNumber] : String.Empty);
+
+                        if (nextLine != String.Empty)
+                        {
+                            int nextLineSubstringStartIndex = startPositionInLine == 0 ? 0 : startPositionInLine - 1;
+                            int nextLineSubstringEndIndex = endPositionInLine == nextLine.Length - 1 ? endPositionInLine : endPositionInLine + 1;
+                            string nextLineSubstring = nextLine.Substring(nextLineSubstringStartIndex, nextLineSubstringEndIndex - nextLineSubstringStartIndex + 1);
+
+                            // Check if contains a special char (all char except dot or number)
+                            if (Regex.IsMatch(nextLineSubstring, @"[^\.0-9]"))
+                            {
+                                total += number;
+                                //Console.WriteLine($"Number : {number} - Start position : {startPositionInLine} - End position : {endPositionInLine}");
+                                continue;
+                            }
+                        }
+
+                    }              
+                }
+                */
+                #endregion Part 1
+                Console.WriteLine("Total : " + total);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error while reading text file");
+                Console.WriteLine(e.Message);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ D4/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputPath = "C:\\Users\\Charles\\Desktop\\Projects\\AdventOfCode2023\\D4\\input.txt";
+
+            List<string> lines = new List<string>();
+
+            Dictionary<int, int> numberOfIterationsOfThisCard = new Dictionary<int, int>();
+
+            int total = 0;
+            try
+            {
+                lines = GetLines(inputPath);
+
+                foreach(string line in lines)
+                {
+
+                    int cardNumber = int.Parse(line.Substring(line.IndexOf(' ') + 1, line.IndexOf(' ') - 1));
+
+                    int iterationForThisCard = (numberOfIterationsOfThisCard.ContainsKey(cardNumber) ? (numberOfIterationsOfThisCard[cardNumber] + 1) : 1);
+
+                    for(int i = 0; i < iterationForThisCard; i++)
+                    {
+                        string lineWithoutCardNumber = line.Substring(line.IndexOf(':') + 1);
+                        string lineWithoutSecondPart = lineWithoutCardNumber.Substring(0, lineWithoutCardNumber.IndexOf('|'));
+                        string lineWithoutFirstPart = lineWithoutCardNumber.Substring(lineWithoutCardNumber.IndexOf('|') + 1);
+
+                        List<int> firstPartNumbers = lineWithoutSecondPart.Split(' ').Where(x => !String.IsNullOrWhiteSpace(x)).Select(int.Parse).ToList();
+                        List<int> secondPartNumbers = lineWithoutFirstPart.Split(' ').Where(x => !String.IsNullOrWhiteSpace(x)).Select(int.Parse).ToList();
+
+                        // Check the number of number that is in the first part that is in the second part
+                        int numberOfNumberInFirstPartThatIsInSecondPart = firstPartNumbers.Intersect(secondPartNumbers).Count();
+                        int numberToAdd = ((int)Math.Pow(2, numberOfNumberInFirstPartThatIsInSecondPart)) / 2;
+
+                        for (int j = 1; j < numberOfNumberInFirstPartThatIsInSecondPart + 1; j++)
+                        {
+                            numberOfIterationsOfThisCard[cardNumber + j] = (numberOfIterationsOfThisCard.ContainsKey(cardNumber + j) ? numberOfIterationsOfThisCard[cardNumber + j] + 1 : 1);
+                        }
+
+                        //total += numberToAdd;
+                        total += 1;
+                        Console.WriteLine($"Card {cardNumber}, total after adding {total}");
+                    }       
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error while reading text file");
+                Console.WriteLine(e.Message);
+            }
+
+            Console.WriteLine("Total: " + total);
+            Console.ReadLine();
+        }
+
+        private static List<string> GetLines(string inputPath)
+        {
+            List<string> lines = new List<string>();
+
+            using (StreamReader sr = new StreamReader(inputPath))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    lines.Add(line);
+                }
+            }
+
+            return lines;
+        }
+    }
+}
